0f12b2cf640bd8ce6b77350a97df10b60ed5e678
unknown <smartbizlord@gmail.com>
2023-10-16 11:31:17 +0100

    finally was able to intercept the request to be able to store the response data

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ function trebbler(req, res, next) {
     // next(mumu)
     return function(req, res, next) {
         // 
+        let oldSend = res.send;
+
+        res.send = function(data) {
+            this.__response = data
+            // console.log(data) // do something with the data
+            // res.send = oldSend // set function back to avoid the 'double-send'
+            // return res.send(data) // just call as normal with data
+            oldSend.call(this, data)
+        }
+        res.on('finish', function() {
+            console.log(res.__response)
+        })
         next()
     }
 }
